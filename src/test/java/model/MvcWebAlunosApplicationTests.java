bd5433fda48d160c89abe89bfc3f9ba3537f1cf0
ttodep <jppedottrocha@gmail.com>
2026-05-27 17:41:03 -0300

    Add files via upload

---
+++ src/test/java/model/MvcWebAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcWebAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
